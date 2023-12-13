792943212833ba1a72e95c668043ed112cc84eae
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2023-12-13 20:07:20 +0100

    Added day13 part1 solution

---
+++ day13/part1.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = path.join(__dirname, 'input.txt');
+
+function getMirrors(data) {
+    let mirrors = [];
+    
+    for(let line = 0; line < data.length; line++) {
+        let currentMirror = [];
+        while(data[line] !== '' && line < data.length) {
+            currentMirror.push(data[line]);
+            line++;
+        }
+        mirrors.push(currentMirror);
+    }
+
+    return mirrors;
+}
+
+function transpose(matrix) {
+    matrix = matrix.map(line => line.split(''));
+
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    let result = [];
+    for (let i = 0; i < cols; i++) {
+        result[i] = [];
+        for (let j = 0; j < rows; j++) {
+            result[i][j] = matrix[j][i];
+        }
+    }
+
+    result = result.map(line => line.join(''));
+
+    return result;
+}
+
+function compareRows(row1,row2) {
+    for(let i = 0; i < row1.length; i++) {
+        if(row1[i] !== row2[i]) return false;
+    }
+    return true;
+}
+
+function scanRows(mirror) {
+    for(let i = 0; i < mirror.length-1; i++) {
+        let isMirror = true;
+
+        let startingIndex = i;
+        
+        let topIdx = i;
+        let bottomIdx = i+1;
+
+        let topRow;
+        let bottomRow;
+
+        while(topIdx >= 0 && bottomIdx < mirror.length) {
+            topRow = mirror[topIdx];
+            bottomRow = mirror[bottomIdx];
+            if(!compareRows(topRow,bottomRow)) {
+                isMirror = false;
+                break;
+            }
+            topIdx--;
+            bottomIdx++;
+        }
+        if(isMirror) return 100*(startingIndex+1);
+    }
+    
+    return 0;
+}
+
+function scanColumns(mirror) {
+    mirror = transpose(mirror);
+
+    for(let i = 0; i < mirror.length-1; i++) {
+        let isMirror = true;
+
+        let startingIndex = i;
+        
+        let topIdx = i;
+        let bottomIdx = i+1;
+
+        let topRow;
+        let bottomRow;
+
+        while(topIdx >= 0 && bottomIdx < mirror.length) {
+            topRow = mirror[topIdx];
+            bottomRow = mirror[bottomIdx];
+            if(!compareRows(topRow,bottomRow)) {
+                isMirror = false;
+                break;
+            }
+            topIdx--;
+            bottomIdx++;
+        }
+        if(isMirror) return startingIndex+1;
+    }
+    
+    return 0;
+}
+
+fs.readFile(inputPath,'utf-8',(err,inputFileData) => {
+    if(err) {
+        console.log(`Cannot open the file: ${inputPath}`);
+    } else {
+        const data = inputFileData.split('\r\n');
+        const mirrors = getMirrors(data);
+
+        let result = 0;
+
+        for(let mirror of mirrors) {
+            result += scanColumns(mirror);
+            result += scanRows(mirror);
+        }
+
+        console.log(result);
+    }
+});
