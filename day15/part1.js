19fed8d06decd60cc9db8c5ed764f4296354a91b
Konrad Romanowski <konrad.romanowski.it@gmail.com>
2023-12-15 17:36:20 +0100

    Added day15 part1 solution

---
+++ day15/part1.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = path.join(__dirname, 'input.txt');
+
+fs.readFile(inputPath,'utf-8',(err,inputFileData) => {
+    if(err) {
+        console.log(`Cannot open the file: ${inputPath}`);
+    } else {
+        const data = inputFileData.split(',');
+
+        const values = data.map(str => {
+            let currentValue = 0;
+            for(let i = 0; i < str.length; i++) {
+                currentValue += str.charCodeAt(i);
+                currentValue *= 17;
+                currentValue %= 256;
+            }
+            return currentValue;
+        });
+
+        const result = values.reduce((sum,num)=>{
+            return sum+=num;
+        },0)
+
+        console.log(result);
+    }
+});
